322c0a8b6059decdc506c229c7cbc4bd85e8e31a
Haoli Yang <hyang77@hawk.iit.edu>
2021-01-10 20:54:19 -0600

    Add sign in with google and sign out functionalities into components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.util';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -33,8 +34,10 @@ class SignIn extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required />
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
-
-                    <CustomButton type="submit">Sign in</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
